157796f9ec9943e0b073d1467b87ec8a14c5314e
DS Garnett Dan Nu <99662000+DS-Garnett-Dan-Nu@users.noreply.github.com>
2023-08-23 15:36:42 +0630

    Update DSI_to_streamlit.py

--- DSI_to_streamlit.py
+++ DSI_to_streamlit.py
@@ -1,11 +1,12 @@
 import streamlit as st 
-import joblib
+import pickle
+#import joblib
 
 # da title
 st.title('Welcome to DSI Fish Weight Calculater')
 st.text("Assignment 1 by DS and Hsi")
 
-# take weight and height
+# take parameters
 l1 = st.number_input("Enter your Length 1 (in cm)")
 l2 = st.number_input("Enter your Length 2 (in cm)")
 l3 = st.number_input("Enter your Length 3 (in cm)")
@@ -15,13 +16,14 @@ w = st.number_input("Enter your Width (in cm)")
 
 X_test = [[l1,l2,l3,h,w]]
 
-model = joblib.load("DSI_fish_model.joblib")
+#model = joblib.load("DSI_fish_model.joblib")
 
+model = pickle.load(open("DS_FISH_MODEL.pkl",'rb'))
 result = model.predict(X_test)
 print(result)
 
 # check button pressed
 if(st.button('Calculate Fish Weight')):
 
-    #print BMI index 
-    st.success("Your Fish Weight Index id {} g.".format(result))
+    #fish weight yay!
+    st.success("Your Fish Weight is {} g.".format(result))
