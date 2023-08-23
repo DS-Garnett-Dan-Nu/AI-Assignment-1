7dc5bd4dfc231095e534bcd0ffd5b5807196f6de
DS Garnett Dan Nu <99662000+DS-Garnett-Dan-Nu@users.noreply.github.com>
2023-08-23 16:19:36 +0630

    Update DSI_to_streamlit.py

--- DSI_to_streamlit.py
+++ DSI_to_streamlit.py
@@ -14,13 +14,27 @@ w = st.number_input("Enter your Width (in cm)")
 
 
 X_test = [[l1,l2,l3,h,w]]
+#X_test = [[12,14,16,5,15]]
 
 model = joblib.load("DSI_fish_model.joblib")
 
 result = model.predict(X_test)
 
+
 # check button pressed
 if(st.button('Calculate Fish Weight')):
 
-    #fish weight yay!
-    st.success("Your Fish Weight is {} g.".format(result[0]))
+    if l1 > 0 and l2 > 0 and l3 > 0 and h > 0 and w > 0:
+
+        #fish weight yay!
+        st.success("Length 1 = {} cm.".format(l1))
+        st.success("Length 2 = {} cm.".format(l2))
+        st.success("Length 3 = {} cm.".format(l3))
+        st.success("Height = {} cm.".format(h))
+        st.success("Width = {} cm.".format(w))
+        st.success("Your Fish Weight is {} g.".format(result[0][0]))
+
+    else:
+
+        st.warning("One of you values is INVALID. Please Try Again.")
+
